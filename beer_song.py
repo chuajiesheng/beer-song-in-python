fa48306129dde7d89f2c431e69b69b73be3a9339
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-01-02 22:46:26 +0800

    Refactor sentence generation

--- beer_song.py
+++ beer_song.py
@@ -1,4 +1,8 @@
 class BeerSong():
+    FIRST_SENTENCE = '{} of beer on the wall, {} of beer.'
+    SECOND_SENTENCE = 'Take one down and pass it around, {} of beer on the wall.'
+    BUY_MORE_BEER = 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+
     def __init__(self):
         pass
 
@@ -11,14 +15,14 @@ class BeerSong():
         else:
             return 'no more bottles'
 
+    @staticmethod
+    def cap(s):
+        return s[0].upper() + s[1:]
+
     def at(self, level):
-        if level > 0:
-            base_lyrics = '{} of beer on the wall, {} of beer.\nTake one down and pass it around, {} of beer on the wall.'
-            return base_lyrics.format(self.bottles(level), self.bottles(level), self.bottles(level - 1))
-        else:
-            base_lyrics = '{} of beer on the wall, {} of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.'
-            no_of_bottles = self.bottles(level)
-            return base_lyrics.format(no_of_bottles[0].upper() + no_of_bottles[1:], no_of_bottles)
+        first_sentence = self.FIRST_SENTENCE.format(self.cap(self.bottles(level)), self.bottles(level))
+        second_sentence = self.SECOND_SENTENCE.format(self.bottles(level - 1)) if level > 0 else self.BUY_MORE_BEER
+        return '\n'.join([first_sentence, second_sentence])
 
     def main(self):
         pass
